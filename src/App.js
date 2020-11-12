73e458c9681b948730eee5a394b7053e1ca82030
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-12 11:03:14 -0800

    added router on app.js

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+    Link,
+} from "react-router-dom";
+import Todos from './Todos.js';
+import Login from './Login.js'
+import SignUp from './SignUp.js'
+import PrivateRoute from './PrivateRoute.js';
+
+export default class App extends Component {
+  state = { token: localStorage.getItem('TOKEN') }
+
+  handleTokenChange = (myToken) => {
+    this.setState({ token: myToken });
+    localStorage.setItem('TOKEN', myToken);
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            { this.state.token && <div>welcome, user!!!</div> }
+            { this.state.token && <Link to="/todos"><div>todos</div></Link> }
+            <Link to="/login"><div>log in</div></Link>
+            <Link to="/signup"><div>sign up</div></Link>
+            <button onClick={() => this.handleTokenChange('')}>logout</button>
+          </ul>
+          <Switch>
+            <Route exact path='/login' render={(routerProps) => <Login 
+                handleTokenChange={this.handleTokenChange} 
+                {...routerProps} />} />
+            <Route 
+            exact path='/signup' 
+              render={(routerProps) => <SignUp 
+                handleTokenChange={this.handleTokenChange} 
+                {...routerProps}/>} />
+            <PrivateRoute 
+              exact 
+              path='/todos' 
+              token={this.state.token} 
+              render={(routerProps) => <Todos 
+              {...routerProps} />} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
